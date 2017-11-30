a5cc00ad76b0096dcd08f2bcb819481df1fa9338
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2017-11-30 17:16:10 -0600

    Add some preliminary neuron geometry parameters

--- src/main.m
+++ src/main.m
@@ -17,3 +17,9 @@ E_Cl = -10.613; % chloride nernst potential (mV)
 gNa = 120e3;    % bulk maximum sodium channel conductance (uS/cm^2)
 gK = 36e3;      % bulk maximum potassium channel conductance (uS/cm^2)
 gCl = 300;      % bulk chloride channel conductance (uS/cm^2)
+
+% Preliminary neuron geometry parameters
+% from wikipedia (specify sources later)
+
+rSoma = 8.75e-4;    % soma radius (cm)
+r = 0.5e-4;         % axon/dendrite radius (cm)
