8f15da1e09c5a0c86e86132dd5d0ce47a357a910
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2017-11-30 13:14:27 -0600

    Fix errors in constructInputMatrix
    Also adds a simple testSim function

--- src/constructInputMatrix.m
+++ src/constructInputMatrix.m
@@ -1,14 +1,18 @@
-function C = constructInputMatrix(connGraph)
+function C = constructInputMatrix(A)
 %CONSTRUCTINPUTMATRIX creates a matrix mapping voltages to neuron inputs
     
+    connGraph = graph(A);
     inputWeights = arrayfun(@(k) sumInputWeights(connGraph,k),...
                                     1:height(connGraph.Nodes));
-    C = A + diag(inputWeights);
+    C = A - diag(inputWeights);
 end
 
-function gSum = sumInputWeights(connGraph, k)
+function gSum = sumInputWeights(G, k)
     gSum = 0;
-    for i = 1:height(connGraph.Nodes)
-        gSum = gSum + findedge(connGraph,i,k);
+    for i = 1:height(G.Nodes)
+        e = findedge(G,i,k);
+        if (e ~= 0)
+            gSum = gSum + G.Edges.Weight(e);
+        end
     end
 end
