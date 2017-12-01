5a11a5896de907d82e107306ff23a68fe7e51aaa
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2017-12-01 15:29:33 -0600

    Added more parameters in main
    Added some further geometric parameters in main

--- src/main.m
+++ src/main.m
@@ -21,16 +21,26 @@ n0 = 0.2787;
 rSoma = 8.75e-4;    % soma radius (cm)
 r = 0.5e-4;         % axon/dendrite radius (cm)
 
+% determined from geometry
+dx = 4.5161e-4;
+
 % Active isopotential cell simulation
-surfArea = 4 * pi * rSoma^2;
-C_m = Cm * surfArea;
-g_Na = gNa * surfArea;
-g_K = gK * surfArea;
-g_Cl = gCl * surfArea;
+surfAreaSoma = 4 * pi * rSoma^2;
+surfAreaSection = pi * r^2 * dx;
+crossArea = pi * r^2;
+C_m_soma = Cm * surfAreaSoma;
+g_Na_soma = gNa * surfAreaSoma;
+g_K_soma = gK * surfAreaSoma;
+g_Cl_soma = gCl * surfAreaSoma;
+C_m_section = Cm * surfAreaSection;
+g_Cl_section = gCl * surfAreaSection;
+g_i_section = gCy * crossArea / dx;
+g_i_soma = 2/3 * gCy * pi * rSoma^3;
 
-isoActive = ActiveSection(C_m,g_Na,g_K,g_Cl,E_Na,E_K,E_Cl);
-isoPassive = PassiveSection(C_m,g_Cl,E_Cl);
-
+section = PassiveSection(C_m_section,g_Cl_section,E_Cl);
+soma = ActiveSection(C_m_soma,g_Na_soma,g_K_soma,g_Cl_soma,...
+                            E_Na,E_K,E_Cl);
+                        
 [t,s] = ode15s(@(t,s) isoActive.dyn(s,istim(t)),[0 5], [V0; h0; m0; n0]);
 plot(t,s);
 
