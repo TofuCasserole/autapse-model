beb9af4a52dca534343b1ac4caeb25475a985f26
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2018-02-11 15:45:13 -0600

    Add colormap and change style of raster plot
    Changes the style of the raster plot to vertical lines, and uses the
    colorcube colormap to color them.

--- src/plotting.m
+++ src/plotting.m
@@ -1,6 +1,21 @@
 hold on;
+height = 0.85;
+rasters = cell(1,length(solutions));
+maxlength = 0;
 for i = 1:length(solutions)
     sol = solutions{i,1};
     raster = rasterize(sol.x,sol.y(1,:));
-    scatter(raster,(i-1) * ones(1,length(raster)),'+','MarkerEdgeColor','k');
+    if length(raster) > maxlength
+        maxlength = length(raster);
+    end
+    rasters{i,1} = raster;
+end
+
+colors = colorcube(maxlength);
+for i = 1:length(rasters)
+    raster = rasters{i,1};
+    for j = 1:length(raster)
+        line(raster(j)*ones(1,2),[(i-1) (i-1+height)],...
+                'Color',colors(j,:),'LineWidth',1.5);
+    end
 end
